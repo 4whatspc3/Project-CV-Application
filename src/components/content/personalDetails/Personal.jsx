ca7d7c47b68a806f53f4fcf31286db9b6b8b0f58
Matheus Augusto <69812698+4whatspc3@users.noreply.github.com>
2025-01-27 22:45:30 -0300

    feat: Add components to 'Personal'
    The added components are: 'Name', 'Email', 'Phone',
    'Address'.

--- src/components/content/personalDetails/Personal.jsx
+++ src/components/content/personalDetails/Personal.jsx
@@ -1,19 +1,87 @@
 import { useState } from "react";
-import { Fragment } from "react";
 
-function Personal () {
-    const [value, setValue] = useState("");
+const Name = ({ handleChange }) => {
+  const [value, setValue] = useState("");
 
-    return (
-        <Fragment>
-            <h2>Personal Details</h2>
+  return (
+    <>
+      <label htmlFor="fullName">Full name: </label>
+      <input
+        name="fullName"
+        id="fullName"
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </>
+  );
+};
 
-            <div>
-                <label htmlFor="fullName">Full name: </label>
-                <input id="fullName" type="text" value={value} onChange={(e) => setValue(e.target.value)}/>
-            </div>
-        </Fragment>
-    )
+const Email = ({ handleChange }) => {
+  const [value, setValue] = useState("");
+
+  return (
+    <>
+      <label htmlFor="email">Email: </label>
+      <input
+        name="email"
+        id="email"
+        type="email"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </>
+  );
+};
+
+const Phone = ({ handleChange }) => {
+  const [value, setValue] = useState("");
+
+  return (
+    <>
+      <label htmlFor="phoneNumber">Phone number: </label>
+      <input
+        name="phoneNumber"
+        type="tel"
+        id="phoneNumber"
+        pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </>
+  );
+};
+
+const Address = ({ handleChange }) => {
+  const [value, setValue] = useState("");
+
+  return (
+    <>
+      <label htmlFor="address">Adress: </label>
+      <input
+        name="address"
+        type="text"
+        id="address"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </>
+  );
+};
+
+function Personal() {
+  return (
+    <>
+      <h2>Personal Details</h2>
+
+      <div>
+        <Name />
+        <Email />
+        <Phone />
+        <Address />
+      </div>
+    </>
+  );
 }
 
 export default Personal;
