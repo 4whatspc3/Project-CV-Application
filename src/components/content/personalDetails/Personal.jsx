a3eeb2c83e5565326e58c3e51a0abe20b6bb6be8
Matheus Augusto <69812698+4whatspc3@users.noreply.github.com>
2025-01-29 14:37:22 -0300

    feat: Wrap 'Personal' components into a form tag.
    Add a placeholder attribute to the components to
    make it easier for the user to understand.

--- src/components/content/personalDetails/Personal.jsx
+++ src/components/content/personalDetails/Personal.jsx
@@ -10,6 +10,7 @@ const Name = ({ handleChange }) => {
         name="fullName"
         id="fullName"
         type="text"
+        placeholder="First and Last Name"
         value={value}
         onChange={(e) => setValue(e.target.value)}
       />
@@ -27,6 +28,7 @@ const Email = ({ handleChange }) => {
         name="email"
         id="email"
         type="email"
+        placeholder="Enter example@email.com"
         value={value}
         onChange={(e) => setValue(e.target.value)}
       />
@@ -45,6 +47,7 @@ const Phone = ({ handleChange }) => {
         type="tel"
         id="phoneNumber"
         pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+        placeholder="Ex. 999-99-999"
         value={value}
         onChange={(e) => setValue(e.target.value)}
       />
@@ -63,6 +66,7 @@ const Address = ({ handleChange }) => {
         type="text"
         id="address"
         value={value}
+        placeholder="City, Country"
         onChange={(e) => setValue(e.target.value)}
       />
     </>
@@ -74,12 +78,12 @@ function Personal() {
     <>
       <h2>Personal Details</h2>
 
-      <div>
+      <form className="personal-details">
         <Name />
         <Email />
         <Phone />
         <Address />
-      </div>
+      </form>
     </>
   );
 }
