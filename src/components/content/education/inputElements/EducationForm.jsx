30d68a9ea3e6cf78446d7f0664333ac18ffbdd6b
Matheus Augusto <69812698+4whatspc3@users.noreply.github.com>
2025-02-07 16:48:54 -0300

    fix: 'EducationForm' component
    The component can now handle cases where
    the user wants to edit existing content
    instead of saving something new.

--- src/components/content/education/inputElements/EducationForm.jsx
+++ src/components/content/education/inputElements/EducationForm.jsx
@@ -8,7 +8,13 @@ import {
   LocationInput,
 } from "./inputElements";
 
-function EducationForm({ onSave, changeValue }) {
+function EducationForm({
+  onSave,
+  changeValue,
+  toEdit,
+  valueOfEdit,
+  changeValueOfEdit,
+}) {
   const [textSchool, setTextSchool] = useState("");
   const [textDegree, setTextDegree] = useState("");
   const [textStart, setTextStart] = useState("");
@@ -36,27 +42,54 @@ function EducationForm({ onSave, changeValue }) {
     setTextLocation("");
   }
 
+  function handleChange(num, e) {
+    if (valueOfEdit !== false) {
+      setTextSchool(toEdit.school);
+      setTextDegree(toEdit.degree);
+      setTextStart(toEdit.start);
+      setTextEnd(toEdit.end);
+      setTextLocation(toEdit.location);
+
+      changeValueOfEdit();
+    } else {
+      switch (num) {
+        case 0:
+          setTextSchool(e.target.value);
+          break;
+        case 1:
+          setTextDegree(e.target.value);
+          break;
+        case 2:
+          setTextStart(e.target.value);
+          break;
+        case 3:
+          setTextEnd(e.target.value);
+          break;
+        case 4:
+          setTextLocation(e.target.value);
+          break;
+      }
+    }
+  }
+
   return (
     <form className="education-form">
       <SchoolInput
         value={textSchool}
-        handleChange={(e) => setTextSchool(e.target.value)}
+        handleChange={(e) => handleChange(0, e)}
       />
       <DegreeInput
         value={textDegree}
-        handleChange={(e) => setTextDegree(e.target.value)}
+        handleChange={(e) => handleChange(1, e)}
       />
       <StartDateInput
         value={textStart}
-        handleChange={(e) => setTextStart(e.target.value)}
+        handleChange={(e) => handleChange(2, e)}
       />
-      <EndDateInput
-        value={textEnd}
-        handleChange={(e) => setTextEnd(e.target.value)}
-      />
+      <EndDateInput value={textEnd} handleChange={(e) => handleChange(3, e)} />
       <LocationInput
         value={textLocation}
-        handleChange={(e) => setTextLocation(e.target.value)}
+        handleChange={(e) => handleChange(4, e)}
       />
       <div>
         <button className="Save" onClick={handleSave}>
