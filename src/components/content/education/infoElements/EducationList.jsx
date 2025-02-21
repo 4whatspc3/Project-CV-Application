9ddeeb032d9a0edb03182a1bb4c82c6fc517cd06
Matheus Augusto <69812698+4whatspc3@users.noreply.github.com>
2025-02-21 11:05:29 -0300

    style: 'education-info' list

--- src/components/content/education/infoElements/EducationList.jsx
+++ src/components/content/education/infoElements/EducationList.jsx
@@ -4,18 +4,23 @@ function EducationList({ educationArray }) {
       <h2>Education</h2>
       {educationArray.map((edc) => (
         <ul key={edc.id}>
-          <li>
-            <span className="text">{edc.school}</span>
-          </li>
-          <li>
-            <span className="text">{edc.degree}</span>
-          </li>
-          <li>
-            <span className="text">{edc.start}</span>
-          </li>
-          <li>
-            <span className="text">{edc.end}</span>
-          </li>
+          <div>
+            <li>
+              <span className="text">{edc.school}</span>
+            </li>
+            <li>
+              <span className="text">{edc.degree}</span>
+            </li>
+          </div>
+          <div>
+            <li>
+              <span className="text">{edc.start}</span>
+            </li>
+            <li>—</li>
+            <li>
+              <span className="text">{edc.end}</span>
+            </li>
+          </div>
           <li>
             <span className="text">{edc.location}</span>
           </li>
